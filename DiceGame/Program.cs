36734057df7f0151439a5b54e282a3c51998ed2e
Roland Käsi <rkasi@tktk.ee>
2021-10-11 12:40:11 +0300

    add new projects

---
+++ DiceGame/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat - arvuti (cpu) ja kasutaja (user)
+            //mõlemad mängijad viskavad täringuid
+            //programm kontrollib, kumb mängija viskas parema tulemuse
+            //mängija, kes sai parema tulemuse on võitja
+            //*täringuid visatakse kolm korda
+            //programm kuulutab võitja
+
+            Random rnd = new Random();
+
+            int cpuScore = 0;
+            int userScore = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                //arvuti vise
+                int cpuRandom = rnd.Next(1, 7);
+                //kasutaja vise
+                int userRandom = rnd.Next(1, 7);
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}, kasutaja viskas {userRandom}");
+
+                if (cpuRandom < userRandom)
+                {
+                    Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+                    userScore = userScore + 1;
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine("Arvuti on mängu võitnud");
+                    cpuScore = cpuScore + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Viik!");
+                }
+            }
+            if(userScore > cpuScore)
+            {
+                Console.WriteLine("Kasutaja on võitja. Palju õnne!");
+            }
+            else if(userScore < cpuScore)
+            {
+                Console.WriteLine("Arvuti on võitja. Palju õnne!");
+            }
+            else
+            {
+                Console.WriteLine("Tulemus on viik!");
+            }
+            
+           
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnuse ja salasõna
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna on "Admin1234"
+            //siis konsoolis kuvatakse "Tere tulemast!"
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti."
+            //kasutajal on kolm katset
+
+            int i = 0;
+
+            while (i < 3)
+            {
+                Console.WriteLine("Sisest kasutajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Sisesta salasõna:");
+                string userPassword = Console.ReadLine();
+
+                if(userName == "admin" && userPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    Console.WriteLine("Palun proovi uuesti.");
+                }
+                if (i == 3)
+                {
+                    Console.WriteLine("Sinu sisselogimis katsed on läbi.");
+                }
+            }
+
+
+
+        }
+    }
+}
